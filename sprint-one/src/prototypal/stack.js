09b7e8d38987aa0ddf7c634ecdbbe5af6a1e76c0
mrabuse <cogglemat@gmail.com>
2017-02-01 17:36:11 -0600

    Finish prototypal stack functionality, but not passing 2 prototype.constructor tests
    For some reason there's a test that says we shouldn't extend the function's prototype, even though the documentation on prototypal functions suggests you should. Will handle this after dinner.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,38 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(Stack.prototype);
+  stack.index = 0;
+  stack.length = 0;
+  stack.storage = {};
+  return stack;
 };
 
 var stackMethods = {};
 
+Stack.prototype.push = function(value) {
+  this.length++;
+  this.storage[this.index] = value;
+  this.index++;
 
+  return value;
+};
+
+Stack.prototype.pop = function() {
+  if (this.length > 0) {
+    this.length--;
+    this.index--;
+  }
+  var popped = this.storage[this.index];
+
+  this.storage[this.index] = undefined;
+
+  return popped;
+};
+
+Stack.prototype.size = function() {
+  return this.length;
+};
+
+
+var stack = Stack();
