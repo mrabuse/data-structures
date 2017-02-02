557ddaf2e7f9871f1a3dafdb4d3c32c32035d6ac
mrabuse <cogglemat@gmail.com>
2017-02-02 12:31:08 -0600

    Add things to tree (not passing all tests)

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,16 @@
+var extend = function (obj1, obj2) {
+  for (var key in obj2) {
+    obj1[key] = obj2[key];
+  }
+};
+
+
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,9 +18,34 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+
+  if (this.value === undefined) {
+    this.value = value;
+  } else {
+    var treeChild = Tree(value);
+    this.children.push(treeChild);
+  }
 };
 
 treeMethods.contains = function(target) {
+  var hasTarget = false;
+  var tree = this;
+  console.log(this);
+
+  var lol = function(tree) {
+    if (tree.value === target) {
+      hasTarget = true;
+    } else {
+      if (tree.children.length > 0) {
+        for (var i = 0; i < tree.children.length; i++) {
+          lol(tree.children[i]);
+        }
+      }
+    }
+
+    lol(tree);
+    return hasTarget;
+  };
 };
 
 
