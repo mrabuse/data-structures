5710403bd277322262c631f825369babf27aa164
mrabuse <cogglemat@gmail.com>
2017-02-02 17:39:17 -0600

    Add tests to confirm additional set functionality

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -24,4 +24,23 @@ describe('set', function() {
     expect(set.contains('Mel Gibson')).to.equal(false);
   });
 
+  it('should count number of instances of added values', function () {
+    set.add('Kristin Bell');
+    set.add('Kristin Bell');
+    expect(set._storage['Kristin Bell']).to.equal(2);
+  });
+
+  it('should reduce the number of instances of a value when it is removed', function () {
+    set.add('Meryl Streep');
+    set.add('Meryl Streep');
+    set.remove('Meryl Streep');
+    expect (set._storage['Meryl Streep']).to.equal(1);
+  });
+
+  it('should remove a value when the number of its instances becomes zero', function () {
+    set.add('Fozzie Bear');
+    expect(set.contains('Fozzie Bear')).to.equal(true);
+    set.remove('Fozzie Bear');
+    expect(set.contains('Fozzie Bear')).to.equal(false);
+  });
 });
