4a1abb3e6b3f7ddfae8743e1dc09e2ede7f99f06
mrabuse <cogglemat@gmail.com>
2017-02-02 17:02:51 -0600

    Complete prototypal set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
+
 };
 
 setPrototype.contains = function(item) {
+  var hasItem = false;
+  if (this._storage[item]) {
+    hasItem = true;
+  }
+  return hasItem;
 };
 
 setPrototype.remove = function(item) {
+  this._storage[item] = undefined;
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ add: O(1)
+ contains: O(1)
+ remove: O(1)
  */
