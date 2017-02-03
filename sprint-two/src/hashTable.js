bb52fea4af319d6d0cd2d30177a56ea50e0a79df
mrabuse <cogglemat@gmail.com>
2017-02-02 20:08:37 -0600

    Complete more of hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,18 +7,39 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  
-  this._storage.set(index, v);
+  var storedArray = this._storage.get(index);
+  var findEmptySpot = function (array) {
+    if (array[2][0] === undefined) {
+      array[2] = [k, v, []];
+    } else {
+      findEmptySpot(array[2][0]);
+    }
+  };
+
+  if (storedArray === undefined) {
+    return this._storage.set(index, [k, v, []]);
+  } else {
+    return findEmptySpot(storedArray);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  var storedValues = this._storage.get(index);
+  var findKey = function (array) {
+    if (array[0] === k) {
+      return array[1];
+    } else {
+      findKey(array[2]);
+    }
+  };
+
+  return findKey(storedValues);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, undefined);
+  return this._storage.set(index, undefined);
 };
 
 
