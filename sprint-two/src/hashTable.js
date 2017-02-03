93904c232f87d57a2f281209c1ab9322e6e1f9bc
mrabuse <cogglemat@gmail.com>
2017-02-03 12:00:57 -0600

    Complete pseudoclassical hashtable and complexities

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,45 +5,85 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
+//Value tuple: array[0] = key, array[1] = value,
+//array[2] = array link to key:value pairs w/ matching hash
+
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var storedArray = this._storage.get(index);
+
   var findEmptySpot = function (array) {
-    if (array[2][0] === undefined) {
+    if (array[2].length === 0) {
       array[2] = [k, v, []];
+    } else if (array[0] === k) {
+      array[1] = v;
     } else {
       findEmptySpot(array[2][0]);
     }
   };
 
   if (storedArray === undefined) {
-    return this._storage.set(index, [k, v, []]);
+    this._storage.set(index, [k, v, []]);
+  } else if (storedArray[0] === k) {
+    storedArray[1] = v;
   } else {
-    return findEmptySpot(storedArray);
+    findEmptySpot(storedArray);
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var storedValues = this._storage.get(index);
+
   var findKey = function (array) {
     if (array[0] === k) {
       return array[1];
     } else {
-      findKey(array[2]);
+      if (array[2].length === 0) {
+        return undefined;
+      } else {
+        return findKey(array[2]);
+      }
     }
   };
 
-  return findKey(storedValues);
+  if (storedValues === undefined) {
+    return undefined;
+  } else {
+    return findKey(storedValues);
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.set(index, undefined);
+  var storedValues = this._storage.set(index, undefined);
+
+  var annihilateK = function (array) {
+    if (array[0] === k) {
+      if (array[2].length === 0) {
+        array = undefined;
+      } else {
+        array = array[2];
+      }
+    } else if (array[2].length === 0) {
+      return;
+    } else {
+      annihilateK(array[2]);
+    }
+  };
+
+  if (storedValues === undefined) {
+    return;
+  } else {
+    annihilateK(storedValues);
+  }
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ insert: O(n)
+ remove: O(n)
+ retrieve: O(n)
  */
