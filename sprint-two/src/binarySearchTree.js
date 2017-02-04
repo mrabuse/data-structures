30efc881eb106368ad50ec8b07c31b2eba4fe3d1
mrabuse <cogglemat@gmail.com>
2017-02-03 20:03:18 -0600

    Add working insert and contains, plus a lot of depth nonsense

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,88 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function (value) {
+  this.value = value;
+  this.left;
+  this.right;
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+  var side;
+  var tree = this;
+
+  var findSpot = function (tree) {
+    if (tree.value > value) {
+      side = 'left';
+    }
+
+    if (tree.value < value) {
+      side = 'right';
+    }
+
+    if (tree[side] === undefined) {
+      tree[side] = new BinarySearchTree(value);
+    } else {
+      findSpot(tree[side]);
+    }
+  };
+
+  findSpot(tree);
+};
+
+
+BinarySearchTree.prototype.contains = function(value) {
+  var hasValue = false;
+  var side;
+  var tree = this;
+
+  var findValue = function (tree) {
+
+    if (tree.value > value) {
+      side = 'left';
+    }
+
+    if (tree.value < value) {
+      side = 'right';
+    }
+
+    if (tree.value === value) {
+      hasValue = true;
+    } else if (tree[side] === undefined) {
+      return;
+    } else {
+      findValue(tree[side]);
+    }
+  };
+
+  findValue(tree);
+  return hasValue;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  var tree = this;
+  var previousTree;
+  var leftNodes = [];
+  var rightNodes = [];
+  var level = 0;
+
+  var eachOne = function (tree) {
+    if (tree.left !== undefined) {
+      if (tree.left.looked === undefined) {
+        leftNodes[level].push({previousLevel: tree, looked: true});
+        cb(tree.left.value);
+        eachOne(tree.left);
+      } else {
+        if (tree.right !== undefined) {
+          if (tree.right.looked === undefined) {
+            rightNodes[level].push({previousLevel: tree, looked: true});
+          }
+        } else if (tree.right === undefined) {
+          eachOne(tree.previousLevel);
+        }
+      }
+    }
+  };
+
+  cb(tree.value);
+  return eachOne(tree);
 };
 
 
