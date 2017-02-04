c15d40f6bb2e9a6a8fbc407ca4253d30588ab327
mrabuse <cogglemat@gmail.com>
2017-02-04 15:36:40 -0600

    Add tree tests for functions traverse and removeFromParent, and property parent

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -5,15 +5,18 @@ describe('tree', function() {
     tree = Tree();
   });
 
-  it('should have methods named "addChild" and "contains", and a property named "value"', function() {
+  it('should have methods named "addChild", "removeFromParent", "contains", "traverse" and properties named "value" and "parent"', function() {
     expect(tree.addChild).to.be.a('function');
     expect(tree.contains).to.be.a('function');
+    expect(tree.removeFromParent).to.be.a('function');
+    expect(tree.traverse).to.be.a('function');
     expect(tree.hasOwnProperty('value')).to.equal(true);
+    expect(tree.hasOwnProperty('parent')).to.equal(true);
   });
 
   it('should add children to the tree', function() {
     tree.addChild(5);
-    expect(tree.children[0].value).to.equal(5);
+    expect(tree.children['5'].value).to.equal(5);
   });
 
   it('should return true for a value that the tree contains', function() {
@@ -28,17 +31,36 @@ describe('tree', function() {
 
   it('should be able to add children to a tree\'s child', function() {
     tree.addChild(5);
-    tree.children[0].addChild(6);
-    expect(tree.children[0].children[0].value).to.equal(6);
+    tree.children['5'].addChild(6);
+    expect(tree.children['5'].children['6'].value).to.equal(6);
   });
 
   it('should correctly detect nested children', function() {
     tree.addChild(5);
     tree.addChild(6);
-    tree.children[0].addChild(7);
-    tree.children[1].addChild(8);
+    tree.children['5'].addChild(7);
+    tree.children['5'].addChild(8);
     expect(tree.contains(7)).to.equal(true);
     expect(tree.contains(8)).to.equal(true);
+  });
+
+  it('should return false for a relationship that was removed', function() {
+    tree.addChild(5);
+    tree.children['5'].addChild(6);
+    expect(tree.children['5'].children['6'].value).to.equal(6);
+    tree.children['5'].removeFromParent(6);
+    expect(tree.children['5'].children['6']).to.be.undefined;
+  });
+
+  it('should perform a callback on every node in the tree', function() {
+    var array = [];
+    var func = function (value) { array.push(value); };
+    tree.addChild(1);
+    tree.addChild(3);
+    tree.children['1'].addChild(2);
+    tree.children['3'].addChild(4);
+    tree.traverse(func);
+    expect(array).to.eql([1, 2, 3, 4]);
   });
 
 });
